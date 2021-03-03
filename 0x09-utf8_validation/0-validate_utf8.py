1ddb33bce430870ef54e65bf2ac13f73734baf2d
duvanjm <duvanjarin@gmail.com>
2021-03-03 15:53:56 -0500

    add task

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -4,20 +4,18 @@
 
 def validUTF8(data):
     """ 0. UTF-8 Validation"""
-    try:
-        isvalid = True
-        for i in data:
-            i = i & 0xff
-            if i >= 192 and i < 224:
-                isvalid = False
-            if i >= 224 and i < 240:
-                isvalid = False
-            if i >= 240 and i < 248:
-                isvalid = False
-            if i >= 128 and i < 192:
-                isvalid = False
-            if i > 255:
-                isvalid = False
-        return(isvalid)
-    except Exception as e:
-        return(False)
+    for n in data:
+        if count == 0:
+            if (n >> 3) == 0b11110:
+                count = 3
+            elif (n >> 4) == 0b1110:
+                count = 2
+            elif (n >> 5) == 0b110:
+                count = 1
+            elif (n >> 7):
+                return False
+        else:
+            if (n >> 6) != 0b10:
+                return False
+            count -= 1
+    return count == 0
