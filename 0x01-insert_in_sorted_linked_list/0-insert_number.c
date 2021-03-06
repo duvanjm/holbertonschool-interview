8d927dcf1d07e5e113f032c5a1ca676e7aa20a44
duvanjm <duvanjarin@gmail.com>
2021-03-06 15:04:19 -0500

    fix stdlib

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -7,7 +7,7 @@
  * Return: the address of the new node, or NULL if it failed
  */
 
-listint_t *insert_node(listint_t **head, int number);
+listint_t *insert_node(listint_t **head, int number)
 {
     listint_t *newnode, *head2;
 
