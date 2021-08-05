b45c96438114e94033158565935e717d5a64fcff
duvanjm <duvanjarin@gmail.com>
2021-08-05 18:41:40 -0500

    fix betty style

--- 0x0E-linear_skip/search.h
+++ 0x0E-linear_skip/search.h
@@ -20,16 +20,15 @@
  */
 typedef struct skiplist_s
 {
-    int n;
-    size_t index;
-    struct skiplist_s *next;
-    struct skiplist_s *express;
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
 } skiplist_t;
 
 skiplist_t *create_skiplist(int *array, size_t size);
 void print_skiplist(const skiplist_t *list);
 void free_skiplist(skiplist_t *list);
 skiplist_t *linear_skip(skiplist_t *head, int value);
-skiplist_t *linear_skip(skiplist_t *list, int value);
 
 #endif /* _SEARCH_H_ */
