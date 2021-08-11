d9c81cce7e3d7e0f90bdefc206ebe0c4fb9699f1
duvanjm <duvanjarin@gmail.com>
2021-08-11 16:53:01 -0500

    fix semistandard

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -5,15 +5,21 @@ const arg = process.argv[2];
 const req = 'https://swapi-api.hbtn.io/api/films/' + arg[0];
 
 request(req, (error, responce, body) => {
+  if (error) {
+    return;
+  }
   const res = JSON.parse(body);
   const namelst = {};
-  res.characters.forEach(char_url => {
-    request(char_url, (error, responce, body) => {
+  res.characters.forEach(charurl => {
+    request(charurl, (error, responce, body) => {
+      if (error) {
+        return;
+      }
       const character = JSON.parse(body);
       namelst[character.url] = character.name;
       if (Object.keys(namelst).length === res.characters.length) {
-        res.characters.forEach(char_url =>
-          console.log(namelst[char_url]));
+        res.characters.forEach(charurl =>
+          console.log(namelst[charurl]));
       }
     });
   });
