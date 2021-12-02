383f39dc4710468494066f95919006b29924550a
duvanjm <duvanjarin@gmail.com>
2021-12-02 14:22:13 -0500

    fix betty style

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -4,7 +4,7 @@
  * wildcmp - function that compares two strings
  * @s1: string 1
  * @s2: string 2
- * Return: returns 1 if the strings can be considered identical, otherwise return 0
+ * Return: 1 if the strings can be considered identical, otherwise return 0
  **/
 
 int wildcmp(char *s1, char *s2)
