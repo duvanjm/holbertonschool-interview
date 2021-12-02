b1038d2766c903e775fc024109c85da0fe2ac9d9
duvanjm <duvanjarin@gmail.com>
2021-12-02 14:09:27 -0500

    add task

---
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * wildcmp - function that compares two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: returns 1 if the strings can be considered identical, otherwise return 0
+ **/
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	else if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	else if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
+	return (0);
+}
